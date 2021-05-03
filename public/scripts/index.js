94cdf5075e490063c3e6c56e735ac43ac9497f57
caio <caiofelicio17@gmail.com>
2021-05-03 10:04:02 -0300

    site quase pronto

---
+++ public/scripts/index.js
@@ -0,0 +1,4 @@
+document.getElementById("home").addEventListener("click", scroll)
+document.getElementById("about").addEventListener("click", scroll)
+document.getElementById("agendamento").addEventListener("click", scroll)
+document.getElementById("contato").addEventListener("click", scroll)
---
+++ src/server.js
@@ -0,0 +1,57 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const nodemailer = require("nodemailer")
+const dotenv = require("dotenv")
+
+const user = "empresara7@gmail.com"
+const pass = "uvjbbgqplwqmokws"
+
+
+
+const app = express()
+
+app.use(express.static("public"))
+    
+    .use(express.static("src/views"))
+
+    .use(bodyParser.urlencoded({extended: false}))
+
+    .use(bodyParser.json())
+
+    
+    .get("/", (req, res) => {
+        return res.render("index.html")
+    })
+
+    .post("/", (req, res) => {
+        const data = {
+            name: req.body.name,
+            email: req.body.email,
+            telefone: req.body.telefone,
+            mensagem: req.body.mensagem,
+        }
+
+        const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            auth: {
+            user,
+            pass
+         }
+        })
+
+        transporter.sendMail({
+            from: user,
+            to: data.email,
+            replyTo: user,
+            subject: "Atendimento Galac",
+            text: `Olá ${data.name}, recebemos sua mensagem, em breve entraremos em contato.`
+        }).then(info => {   
+            console.log("enviado")
+            res.redirect("/")
+        }).catch(error => {
+            res.redirect("/")
+        })
+    })
+
+    .listen(5500, () => { console.log("\nServidor iniciado na porta 5500. http://localhost:5500") })
