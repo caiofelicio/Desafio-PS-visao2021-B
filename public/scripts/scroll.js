17632cdd20d115dddfa653ff3367695470296a1c
caio <caiofelicio17@gmail.com>
2021-05-09 02:47:57 -0300

    mudanças no css, particionamento do css, mudanças no slide, etc...

--- public/scripts/scroll.js
+++ public/scripts/scroll.js
@@ -2,7 +2,7 @@ $('nav a').click(function(e){
 	e.preventDefault();
 	var id = $(this).attr('href'),
 			menuHeight = $('nav').innerHeight(),
-			targetOffset = $(id).offset().top;
+			targetOffset = $(id).offset().top
 	$('html, body').animate({
 		scrollTop: targetOffset - menuHeight
 	}, 500);
